2549152df2e2cd7070895d6bf493bb5e1139f414
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-19 11:14:36 +0000

    create a tree printed out to console.

--- Tree.java
+++ Tree.java
@@ -5,6 +5,11 @@ public class Tree {
     public static void main(String[] args) {
         // this exercise is going to introduce print a tree to the screen.
         System.out.println("Hello, that is first day of learning JAVA.");
+        System.out.println("Ready to get my hands dirty!");
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println("*********");
+        System.out.println("___| |____");
     }
-
 }
