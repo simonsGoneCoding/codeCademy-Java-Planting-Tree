b66198aa43f12c6108b7b5a86386ba031d9af625
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-19 11:06:25 +0000

    Create first class Tree.

---
+++ Tree.java
@@ -0,0 +1,10 @@
+/**
+ * Tree
+ */
+public class Tree {
+    public static void main(String[] args) {
+        // this exercise is going to introduce print a tree to the screen.
+        System.out.println("Hello, that is first day of learning JAVA.");
+    }
+
+}
